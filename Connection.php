ce4b42d684f7fe7d84e6a1592533fadc3c53dd5f
vixoja <donmoyaahumda@gmail.com>
2021-02-05 22:32:08 -0300

    0.5

--- Connection.php
+++ Connection.php
@@ -2,8 +2,8 @@
 
     //Connection to mysql server, where you should get your own database
     $link = 'mysql:localhost=host;dbname=Access_Portal_Web';
-    $user_mysql = 'vicente';
-    $password_mysql = 'hola1234';
+    $user_mysql = 'vicente';//change for your own mysql username
+    $password_mysql = 'hola1234';//change for your own mysql userpassword
 
     //PDO Give you a bridge. If all go wrong, its send you an error message xd
     try{
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="hola.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="nav">
+            <div class="nav_logo">Silver Waffle</div>
+            <div class="nav_item"><a href="#">Home</a></div>
+            <div class="nav_item"><a href="#">Features</a></div>    
+            <div class="nav_item"><a href="Login.php">Login</a></div>
+            <div class="nav_item"><a href="#">Contact</a></div>
+        </div>
+
+        <div class="tittle">
+            <div class="tittle__h1">
+                <h1>Welcome to Silver Waffle</h1>
+            </div>    
+            <div class="tittle__button">
+                <button>Start</button>
+            </div>
+        </div>
+
+        <div class="main">
+            <div class="main__info">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ab vero, distinctio ad totam sit fugit iste magni autem placeat culpa! Reprehenderit cupiditate inventore velit eum deserunt doloribus quasi excepturi.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsa perspiciatis, rem illo molestias, maxime, mollitia aliquam velit ducimus porro soluta! Numquam voluptatum ipsum id consectetur? Est tempore quaerat blanditiis?</p>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
